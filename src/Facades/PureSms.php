499a96a9425ba9a02578c7c137785101da08adda
Mendel Kahan <MENDELKAHAN@users.noreply.github.com>
2025-06-29 03:16:19 +0100

    Update PureSms.php

--- src/Facades/PureSms.php
+++ src/Facades/PureSms.php
@@ -1,13 +1,32 @@
 <?php
+namespace Puresms\Laravel\Channels;
 
-namespace Puresms\Laravel\Facades;
+use Illuminate\Notifications\Notification;
+use Puresms\Laravel\Facades\PureSms;
+use Puresms\Laravel\Models\SmsLog;
 
-use Illuminate\Support\Facades\Facade;
-
-class PureSms extends Facade
+class PureSmsChannel
 {
-    protected static function getFacadeAccessor()
+    /**
+     * Send the given notification.
+     * 
+     * @param  mixed  $notifiable
+     * @param  \Illuminate\Notifications\Notification  $notification
+     * @return void
+     */
+    public function send($notifiable, Notification $notification)
     {
-        return 'puresms';
+        // Get the phone number field from config (defaults to 'sms_number')
+        $phoneField = config('puresms.mobile_number', 'sms_number');
+        
+        // Get the phone number and model ID for logging
+        $recipientPhone = $notifiable->$phoneField ?? null;
+        $recipientId = $notifiable->id; // Assuming the notifiable model has an id attribute
+
+        // Get the message content from the notification
+        $message = $notification->toSms($notifiable);
+
+        // Send SMS using the PureSms package, passing recipient ID
+        $response = PureSms::sendSms($recipientPhone, $message, null, $recipientId);
     }
 }
