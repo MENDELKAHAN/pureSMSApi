563331cc2e14b79889f6fd103f5e89e934dbb868
Mendel Kahan <mendy@famkahan.com>
2026-05-25 16:54:20 +0100

    Fix package route registrations

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SmsController;
+use Puresms\Laravel\Http\Controllers\SmsController;
+use Puresms\Laravel\Http\Controllers\WebhookController;
 
 
 Route::get('/send-sms', [SmsController::class, 'sendSms']);
-Route::post('/send-bulk-sms', [SmsController::class, 'sendBulkSms']);
-Route::get('/sms-balance', [SmsController::class, 'getBalance']);
-Route::delete('/cancel-sms', [SmsController::class, 'cancelSms']);
-Route::delete('/cancel-bulk-sms', [SmsController::class, 'cancelBulkSms']);
+Route::post('/puresms-webhook', [WebhookController::class, 'handleWebhook']);
 
-Route::post('/puresms-webhook', [SmsController::class, 'handleWebhook']);
 
